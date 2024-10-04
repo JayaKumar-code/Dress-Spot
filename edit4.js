d31b5eca8cfcb79479fbc62bea2255bdbcda4943
JayaKumar-code <jaikumar2k3@gmail.com>
2024-10-04 22:09:15 +0530

    Add files via upload

---
+++ edit4.js
@@ -0,0 +1,8 @@
+function changes(){
+    document.getElementById('chan').src = 'https://source.unsplash.com/220x220/?perfume?6'
+    document.getElementById('chang').src = 'https://source.unsplash.com/220x220/?cricket?5'
+    document.getElementById('c').src = 'https://source.unsplash.com/220x220/?football?4'
+    document.getElementById('ch').src = 'https://source.unsplash.com/220x220/?tennis?3'
+    document.getElementById('cha').src = 'https://source.unsplash.com/220x220/?marrige?2'
+    document.getElementById('chann').src = 'https://source.unsplash.com/220x220/?perfume?1'
+}
